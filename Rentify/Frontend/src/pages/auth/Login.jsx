28557c0e3e8af04ecc39d757fec323b837c57939
Anand Reddy <156296146+Anand5d7@users.noreply.github.com>
2024-05-24 21:20:50 +0530

    Add files via upload

---
+++ Rentify/Frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,142 @@
+import React, { useState,useEffect } from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login,checkAuthentication } from "../../redux/auth/authActions";
+import {
+  Paper,
+  TextField,
+  Button,
+  Typography,
+  Container,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+
+const Login = ({ login, error }) => {
+  const navigateTo = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    role: "user", // Default role value
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(formData, navigateTo);
+  };
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await dispatch(checkAuthentication());
+    };
+
+    fetchData();
+  }, [dispatch]);
+  useEffect(() => {
+    if (user) {
+      navigateTo("/");
+    }
+  }, [user]);
+  return (
+    <Container
+      style={{
+        height: "90vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Paper
+        elevation={3}
+        style={{
+          width: 400,
+          padding: 20,
+        }}
+      >
+        <Typography variant="h5" gutterBottom>
+          Login
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            value={formData.email}
+            onChange={handleChange}
+            label="Email"
+            type="email"
+            name="email"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+          />
+          <TextField
+            label="Password"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+          />
+          {/* Role selection */}
+          <FormControl fullWidth variant="outlined" margin="normal">
+            <InputLabel id="role-label">Role</InputLabel>
+            <Select
+              labelId="role-label"
+              id="role"
+              name="role"
+              value={formData.role}
+              onChange={handleChange}
+              label="Role"
+            >
+              <MenuItem value="buyer">Buyer</MenuItem>
+              <MenuItem value="seller">Seller</MenuItem>
+            </Select>
+          </FormControl>
+          {/* End of Role selection */}
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            style={{ marginTop: 10 }}
+          >
+            Login
+          </Button>
+          {error && (
+            <Typography style={{ color: "red", marginTop: 10 }}>
+              {error.message}
+            </Typography>
+          )}
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  loading: state.auth.loading,
+  error: state.auth.error,
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+const mapDispatchToProps = {
+  login,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
